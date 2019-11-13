c5119f943e4d5bae199e5d9393daf4981fb49bd4
Tomash Khamlai <tomash.khamlai@gmail.com>
2019-11-13 04:45:09 +0200

    Application Singleton
    Signed-off-by: Tomash Khamlai <tomash.khamlai@gmail.com>

--- app/Core/App.php
+++ app/Core/App.php
@@ -4,4 +4,19 @@ final class Application {
     private function __construct()
     {
     }
+
+    private function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+    }
+
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    private function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
 }
